2bc6975b800417218f1c8f0a79ca2fbad26517bd
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-05-21 15:35:40 -0600

    project-1.spec.js tests

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -5,13 +5,13 @@ describe('default', () => {
     it('run the tests', () => {});
 });
 
-describe('multipy(x * 10)', function () {
+describe('multipy(x * 10)', function() {
     it('should return the product of the two arguments', function () {
     })
    }
 );
 
-describe('subtract(x - 5)', function () {
+describe('subtract(x - 5)', function() {
     it('should return the difference of the two arguments', function () {
      });
 });
@@ -25,6 +25,38 @@ describe('areEqual(x, y)', function() {
     it('should return true if the arguments are equal', function() {
     });
 });
+
+describe('lessThanNinety(num)', function() {
+    it('should return true if the argument is less than ninety', function() {
+    });
+});
+
+describe('greaterThanFifty(num)', function() {
+    it('should return true if the argument is greater than fifty', function(){
+     });
+});
+
+describe('add(x, y)', function() {
+    it('should return the sum of the two arguments', function() {
+    });
+});
+
+describe('subtract(x, y)', function() {
+    it('should return the difference of the two arguments', function() {
+    });
+});
+
+describe('divide(x, y)', function() {
+    it('should return the quotient of the two arguments', function() {
+    });
+});
+
+describe('multiply(x, y)', function() {
+    it('should return the product of the two arguments', function() {
+    });
+});
+
+
 
 
 
