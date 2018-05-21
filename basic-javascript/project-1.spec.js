03d720758977e51ed506a8d754921e7940ca45e2
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-05-21 15:09:09 -0600

    writing project-1.spec.js tests

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,30 @@
-const funcs = require('../src/project-1');
+const funcs = require('./project-1');
 
 // write your tests here
+describe('default', () => {
+    it('run the tests', () => {});
+});
+
+describe('multipy(x * 10)', function () {
+    it('should return the product of the two arguments', function () {
+    })
+   }
+);
+
+describe('subtract(x - 5)', function () {
+    it('should return the difference of the two arguments', function () {
+     });
+});
+
+describe('areSameLength(str1, str2)', function() {
+    it('should return true if the arguments have the same length', function() {
+    });
+});
+
+describe('areEqual(x, y)', function() {
+    it('should return true if the arguments are equal', function() {
+    });
+});
+
+
+
