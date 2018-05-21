8aac05cc87e13dac3fbcc1bd9d587547f91ccb04
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-05-21 17:46:20 -0600

    Completed project-2.spec.js tests

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -3,118 +3,118 @@ const funcs = require('./project-2');
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 
 describe('getBiggest(x, y)', function () {
-    it('should return x if it is larger than y', function () {
+     it('should return x if it is larger than y', function () {
     });
-    it('should return y if it is larger than x', function () {
+     it('should return y if it is larger than x', function () {
     });
-    it('should return either one if they are the same', function () {
+     it('should return either one if they are the same', function () {
     });
 });
 
 describe('greeting(language)', function () {
-    it('should return \'Guten Tag!\' for German', function () {
+     it('should return \'Guten Tag!\' for German', function () {
     });
-    it('should return \'Hola!\' for Spanish', function () {
+     it('should return \'Hola!\' for Spanish', function () {
     });
-    it('should return \'Hello!\' for English', function () {
+     it('should return \'Hello!\' for English', function () {
     });
 });
 
 describe('isTenOrFive(num)', function () {
-    it('should return true if num is 10 or 5', function () {
+     it('should return true if num is 10 or 5', function () {
     });
-    it('should return false if num is not 10 or 5', function () {
+     it('should return false if num is not 10 or 5', function () {
     });
 });
 
 describe('isInRange(num)', function () {
-    it('should return true if num is inside range', function () {
+     it('should return true if num is inside range', function () {
     });
-    it('should return false if outside of range', function () {
+     it('should return false if outside of range', function () {
     });
 });
 
 describe('isInteger(num)', function () {
-    it('should return true if num is 5', function () {
+     it('should return true if num is 5', function () {
     });
-    it('should return false if num is 0.5', function () {
+     it('should return false if num is 0.5', function () {
     });
-    it('should return true if num is -20', function () {
+     it('should return true if num is -20', function () {
     });
-    it('should return true for 0', function () {
+     it('should return true for 0', function () {
     });
 });
 
 describe('fizzBuzz(num)', function () {
-    it('should return fizz if divisible by 3', function () {
+     it('should return fizz if divisible by 3', function () {
     });
-    it('should return buzz if divisible by 5', function () {
+     it('should return buzz if divisible by 5', function () {
     });
-    it('should return fizzbuzz if divisible by 3 and 5', function () {
+     it('should return fizzbuzz if divisible by 3 and 5', function () {
     });
-    it('should return num if not divisible by 3 or 5', function () {
+     it('should return num if not divisible by 3 or 5', function () {
     });
 });
 
 describe('isPrime(num)', function () {
-    it('should return true if num is prime', function () {
+     it('should return true if num is prime', function () {
     });
-    it('should return false if num is not prime', function () {
+     it('should return false if num is not prime', function () {
     });
 });
 
 describe('returnFirst(arr)', function() {
-    it('should return the first item in the array', function() {
+     it('should return the first item in the array', function() {
     });
   });
 
   describe('returnLast(arr)', function() {
-    it('should return the last item in the array', function() {
+     it('should return the last item in the array', function() {
     });
   });
 
   describe('getArrayLength(arr)', function() {
-    it('should return the length of the array', function() {
+     it('should return the length of the array', function() {
     });
   });
 
   describe('incrementByOne(arr)', function() {
-    it('should return the array with each value incremented by one', function() {
+     it('should return the array with each value incremented by one', function() {
     });
   });
 
   describe('addItemToArray(arr, item)', function() {
-    it('should return the array with the item added to the end', function() {
+     it('should return the array with the item added to the end', function() {
     });
   });
 
   describe('addItemToFront(arr, item)', function() {
-    it('should return the array with the item added to the front', function() {
+     it('should return the array with the item added to the front', function() {
     });
   });
 
   describe('wordsToSentence(words)', function() {
-    it('should return a string that has all of the words from the array separated by spaces', function() {
+     it('should return a string that has all of the words from the array separated by spaces', function() {
     });
   });
 
   describe('contains(arr, item)', function() {
-    it('should return true if the array contains the item', function() {
+     it('should return true if the array contains the item', function() {
     });
   });
 
   describe('addNumbers(numbers)', function() {
-    it('should add all of the numbers in the array together and return the sum', function() {
+     it('should add all of the numbers in the array together and return the sum', function() {
     });
   });
 
   describe('averageTestScore(testScores)', function() {
-    it('should return the average test score', function() {
+     it('should return the average test score', function() {
     });
   });
 
   describe('largestNumber(numbers)', function() {
-    it('should return the largest number', function() {
+     it('should return the largest number', function() {
     });
   });
 
