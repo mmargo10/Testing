6b14f6f13da8dff8e605435188aec58a7348ac1c
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-05-21 16:33:08 -0600

    Completed project-1.spec.js tests

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -56,6 +56,82 @@ describe('multiply(x, y)', function() {
     });
 });
 
+describe('getRemainder(x, y)', function() {
+    it('should return the division remainder of the two arguments', function() {
+    });
+});
+
+describe('isEven(num)', function() {
+    it('should return the bool true if the argument is even, false otherwise', function() {
+    });
+});
+
+describe('isOdd(num)', function() {
+    it('should return the bool true if the argument is odd, false otherwise', function() {
+    });
+});
+
+describe('square(num)', function() {
+    it('should return the squared argument', function() {
+    });
+});
+
+describe('cube(num)', function() {
+    it('should return the cubed argument', function() {
+    });
+});
+
+describe('raiseToPower(num, exponent)', function() {
+    it('should return the argument after raising it to the exponent power', function() {
+    });
+});
+
+describe('roundNumber(num)', function() {
+    it('should return the argument after rounding it', function() {
+    });
+});
+
+describe('roundUp(num)', function() {
+    it('should return the argument after rounding it up', function() {
+    });
+});
+
+describe('addExclamationPoint(str)', function() {
+    it('should add an exclamation point to the end of the string', function() {
+    });
+});
+
+describe('combineNames(firstName, lastName)', function() {
+    it('should return the two strings combined and separated by a space', function() {
+    });
+});
+
+describe('getGreeting(name)', function() {
+    it('should return the string \'Hello {name}!\'', function() {
+     });
+});
+
+describe('getRectangleArea(length, width)', function() {
+    it('should return the correct area', function() {
+    });
+});
+
+describe('getTriangleArea(base, height)', function() {
+    it('should return the correct area', function() {
+    });
+});
+
+describe('getCircleArea(radius)', function() {
+    it('should return the correct area', function() {
+    });
+});
+
+describe('getRectangularPrismVolume(length, width, height)', function() {
+    it('should return the correct volume', function() {
+    });
+});
+
+
 
 
 
