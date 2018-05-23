de4747e22c0431efb474fb2d156fe37256fab6bb
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-05-23 17:55:32 -0600

    Completed advanced javascript

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -1,4 +1,4 @@
-const arrayFunctions = require('../src/arrays');
+const arrayFunctions = require('./arrays');
 
 // we've gone ahead and gotten a start here for you,
 // except, for some reason, none of our current assertions are working.
@@ -11,7 +11,65 @@ describe('Arrays', () => {
   describe('map', () => {
     it('should be a function', () => {
       const map = arrayFunctions.map;
-      expect(typeof map).toBe('object');
+      expect(typeof map).toBe('function');
+    });
+  });
+
+
+  describe("map", () => {
+    const cb = item => item + 2;
+    const mappedArray = arrayFunctions.map([1, 2, 3], cb);
+    it("Should return an array", () => {
+      expect(mappedArray instanceof Array).toBeTruthy();
+    });
+    it("Should return expected outcome", () => {
+      expect(mappedArray).toEqual([3, 4, 5]);
+    });
+  });
+
+  describe("reduce", () => {
+
+    const elements = [1, 2, 3];
+    const memo = elements.shift();
+    const cb = (memo, item) => {
+      return memo + item;
+    };
+    const reduce = arrayFunctions.reduce(elements, cb, memo);
+    it("Should return expected outcome", () => {
+      expect(reduce).toBe(6);
+    });
+  });
+
+  describe("find", () => {
+
+    const elements = [1, 2, 3];
+    const cb = elements => {
+      return elements < 2;
+    };
+    const find = arrayFunctions.find(elements, cb);
+    it("Should return expected outcome", () => {
+      expect(find).toBe(1);
+    });
+  });
+
+  describe("filter", () => {
+
+    const elements = [4, 5, 6];
+    const cb = (item) => {
+      return item < 5;
+    };
+    const filter = arrayFunctions.filter(elements, cb);
+    it("Should return expected outcome", () => {
+      expect(filter).toEqual([4]);
+    });
+  });
+
+  describe("flatten", () => {
+
+    const elements = [1, 2, 3, [4, 5, 6]];
+    const flatten = arrayFunctions.flatten(elements);
+    it("Should return expected outcome", () => {
+      expect(flatten).toEqual([1, 2, 3, 4, 5, 6]);
     });
   });
 });
--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,103 +1,164 @@
 const funcs = require('./project-1');
 
-// write your tests here
-describe('default', () => {
-    it('run the tests', () => {});
-});
+// arrange
+// act
+// assert
 
-describe('multipy(x * 10)', function() {
+//describe('default', () => {
+//    it('run the tests', () => {});
+// });
+
+describe('multiplyByTen function', () => {
+    const product = 60; 
+    const multiplyByTen = funcs.multiplyByTen(6);
     it('should return the product of the two arguments', function () {
-    })
+        expect(multiplyByTen).toBe(product);
+    });
    }
 );
 
-describe('subtract(x - 5)', function() {
+describe('subtractFive function', () => {
+    const difference = 5;
+    const subtractFive = funcs.subtractFive(10);
     it('should return the difference of the two arguments', function () {
+        expect(subtractFive).toBe(difference);
      });
 });
 
-describe('areSameLength(str1, str2)', function() {
+describe('areSameLength function', () => {
+    const equal = true;
+    const areSameLength = funcs.areSameLength('one', 'two');
     it('should return true if the arguments have the same length', function() {
+        expect(areSameLength).toBe(equal);
     });
 });
 
 describe('areEqual(x, y)', function() {
+    const equal = true;
+    const areEqual = funcs.areEqual(20, 20);
     it('should return true if the arguments are equal', function() {
+        expect(areEqual).toBe(equal);
     });
 });
 
 describe('lessThanNinety(num)', function() {
+    const expected = true;
+    const lessThanNinety = funcs.lessThanNinety(85);
     it('should return true if the argument is less than ninety', function() {
+        expect(lessThanNinety).toBe(expected);
     });
 });
 
 describe('greaterThanFifty(num)', function() {
-    it('should return true if the argument is greater than fifty', function(){
+    const expected = true;
+    const greaterThanFifty = funcs.greaterThanFifty(61);
+    it('should return true if the argument is greater than fifty', function() {
+        expect(greaterThanFifty).toBe(expected);
      });
 });
 
 describe('add(x, y)', function() {
+    const sum = 4;
+    const add = funcs.add(2, 2);
     it('should return the sum of the two arguments', function() {
+        expect(add).toBe(sum);
     });
 });
 
 describe('subtract(x, y)', function() {
+    const difference = 2;
+    const subtract = funcs.subtract(4, 2);
     it('should return the difference of the two arguments', function() {
+        expect(subtract).toBe(difference);
+      
     });
 });
 
 describe('divide(x, y)', function() {
+    const quotient = 5;
+    const divide = funcs.divide(10, 2);
     it('should return the quotient of the two arguments', function() {
+        expect(divide).toBe(quotient);
     });
 });
 
 describe('multiply(x, y)', function() {
+    const product = 20;
+    const multiply = funcs.multiply(10, 2);
     it('should return the product of the two arguments', function() {
+        expect(multiply).toBe(product);
     });
 });
 
 describe('getRemainder(x, y)', function() {
+    const remainder = 1;
+    const getRemainder = funcs.getRemainder(15, 2);
     it('should return the division remainder of the two arguments', function() {
+        expect(remainder).toBe(remainder);
     });
 });
 
 describe('isEven(num)', function() {
+    const expected = true;
+    const isEven = funcs.isEven(10);
     it('should return the bool true if the argument is even, false otherwise', function() {
+        expect(isEven).toBe(expected);
     });
 });
 
 describe('isOdd(num)', function() {
+    const expected = true;
+    const isOdd = funcs.isOdd(7);
     it('should return the bool true if the argument is odd, false otherwise', function() {
+        expect(isOdd).toBe(expected);
     });
 });
 
 describe('square(num)', function() {
+    const expected = 100;
+    const square = funcs.square(10);
     it('should return the squared argument', function() {
+        expect(square).toBe(expected);
     });
 });
 
 describe('cube(num)', function() {
+    const expected = 1728;
+    const cube = funcs.cube(12);
     it('should return the cubed argument', function() {
+        expect(cube).toBe(expected);
     });
 });
 
 describe('raiseToPower(num, exponent)', function() {
+    const expected = 1000;
+    const raiseToPower = funcs.raiseToPower(10, 3);
     it('should return the argument after raising it to the exponent power', function() {
+        expect(raiseToPower).toBe(expected);
     });
 });
 
 describe('roundNumber(num)', function() {
+    const expected = 50;
+    const roundNumber = funcs.roundNumber(49.7);
     it('should return the argument after rounding it', function() {
+        expect(roundNumber).toBe(expected);
     });
 });
 
 describe('roundUp(num)', function() {
+    const expected = 2;
+    const roundUp = funcs.roundUp(1.2);
     it('should return the argument after rounding it up', function() {
+        expect(roundUp).toBe(expected);
     });
 });
 
 describe('addExclamationPoint(str)', function() {
+    const expected = "Voila!";
+    const addExclamationPoint = funcs.addExclamationPoint("Voila");
     it('should add an exclamation point to the end of the string', function() {
+        expect(addExclamationPoint).toBe(expected);
     });
 });
 
@@ -107,27 +168,42 @@ describe('combineNames(firstName, lastName)', function() {
 });
 
 describe('getGreeting(name)', function() {
+    const expected = "Hello name!";
+    const getGreeting = funcs.getGreeting("name");
     it('should return the string \'Hello {name}!\'', function() {
+        expect(getGreeting).toBe(expected);
      });
 });
 
 describe('getRectangleArea(length, width)', function() {
+    const expected = 24;
+    const getRectangleArea = funcs.getRectangleArea(6, 4);
     it('should return the correct area', function() {
+        expect(getRectangleArea).toBe(expected);
     });
 });
 
 describe('getTriangleArea(base, height)', function() {
+    const expected = 80;
+    const getTriangleArea = funcs.getTriangleArea(20, 8);
     it('should return the correct area', function() {
+        expect(getTriangleArea).toBe(expected);
     });
 });
 
 describe('getCircleArea(radius)', function() {
+    const expected = Math.PI * 10 * 10;
+    const getCircleArea = funcs.getCircleArea(10);
     it('should return the correct area', function() {
+        expect(getCircleArea).toBe(expected);
     });
 });
 
 describe('getRectangularPrismVolume(length, width, height)', function() {
+    const expected = 100;
+    const getRectangularPrismVolume = funcs.getRectangularPrismVolume(5, 5, 4);
     it('should return the correct volume', function() {
+        expect(getRectangularPrismVolume).toBe(expected);
     });
 });
 
